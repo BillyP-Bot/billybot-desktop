d1b649cd12e15f9da88c1809320ba92d72f7919e
Chris Leveille <cdleveille@gmail.com>
2023-04-18 23:22:30 -0400

    js -> ts

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+export const Config = {
+	LOCAL: {
+		DASHBOARD_URL: "http://localhost:3000/user",
+		OPEN_DEV_PANEL: true
+	},
+	PRODUCTION: {
+		DASHBOARD_URL: "https://billybot.vercel.app/user",
+		OPEN_DEV_PANEL: false
+	}
+};
